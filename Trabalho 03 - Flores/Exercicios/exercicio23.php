7503b23e6eede55368f508583eb790fba402e69b
Gabriel Grendel <137903969+gab-felipe-2008@users.noreply.github.com>
2026-09-18 18:46:51 -0300

    Calculate triangle area based on base and height

--- Trabalho 03 - Flores/Exercicios/exercicio23.php
+++ Trabalho 03 - Flores/Exercicios/exercicio23.php
@@ -13,4 +13,14 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia o valor da base e da altura de um triangulo e calcule e exiba a sua area (Formula: Area = (Base * Altura) / 2).
 *******************************************************************************/
+$base = 10.0;
 
+$altura = 6.0;
+
+$area = ($base * $altura) / 2;
+
+$resultadoFormatado = number_format($area, 2, ',', '.');
+
+echo "A área do triângulo é: $resultadoFormatado\n";
+/*Resumindo:Esse sistema lê o valor da base e da altura de um triângulo, calcula a área utilizando a fórmula padrão e exibe o resultado obtido.*/
+?>
