668f78a954b1ffe5e18bbecf6318e371ef0d4c4e
Gabriel Grendel <137903969+gab-felipe-2008@users.noreply.github.com>
2026-09-18 18:45:40 -0300

    Update exercicio20.php

--- Trabalho 03 - Flores/Exercicios/exercicio20.php
+++ Trabalho 03 - Flores/Exercicios/exercicio20.php
@@ -13,4 +13,15 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia uma data no formato ddmmaa (ex: 250826) e exiba a mesma data reorganizada no formato mmddaa.
 *******************************************************************************/
+$dataOriginal = "120108";
 
+$dia = substr($dataOriginal, 0, 2);
+$mes = substr($dataOriginal, 2, 2);
+$ano = substr($dataOriginal, 4, 2);
+
+$dataReorganizada = $mes . $dia . $ano;
+
+echo "Data original: $dataOriginal\n";
+echo "Data reorganizada (mmddaa): $dataReorganizada\n";
+/*Resumindo:Esse sistema lê uma string de data no formato ddmmaa, extrai separadamente o dia, o mês e o ano, e os junta em uma nova ordem para exibi-la no formato mmddaa.*/
+?>
