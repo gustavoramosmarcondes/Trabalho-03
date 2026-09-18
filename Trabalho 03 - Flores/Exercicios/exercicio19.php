e8bfc9599a01d86d75ce81b9da3cf6a58ee94919
Gabriel Grendel <137903969+gab-felipe-2008@users.noreply.github.com>
2026-09-18 18:45:11 -0300

    Implement date extraction from string format
    Added code to extract and display day, month, and year from a date string.

--- Trabalho 03 - Flores/Exercicios/exercicio19.php
+++ Trabalho 03 - Flores/Exercicios/exercicio19.php
@@ -13,4 +13,13 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia uma string correspondente a uma data no formato ddmmaa (6 digitos, ex: 250826) e exiba o dia, mes e ano de forma separada.
 *******************************************************************************/
+$data = "120108";
+$dia = substr($data, 0, 2);
+$mes = substr($data, 2, 2);
+$ano = substr($data, 4, 2);
 
+echo "Dia: $dia\n";
+echo "Mês: $mes\n";
+echo "Ano: $ano\n";
+/*Resumindo:Este sistema lê uma string de 6 dígitos de data e utiliza a função de fatiamento para extrair e exibir separadamente os dois primeiros caracteres como dia, os dois do meio como mês e os dois últimos como ano.*/
+?>
