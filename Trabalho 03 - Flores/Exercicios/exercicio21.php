1544049f1620d277e861ffe908b88f665b55bc43
Gabriel Grendel <137903969+gab-felipe-2008@users.noreply.github.com>
2026-09-18 18:46:06 -0300

    Implement weighted average calculation for four numbers
    The program calculates the weighted average of four integers with specified weights.

--- Trabalho 03 - Flores/Exercicios/exercicio21.php
+++ Trabalho 03 - Flores/Exercicios/exercicio21.php
@@ -13,4 +13,20 @@ Componentes:
 Data: 19 de Setembro de 2026
 Descritivo: Escreva um programa que leia quatro numeros inteiros e calcule a media ponderada deles, considerando que os pesos associados a cada numero sao, respectivamente, 1, 2, 3 e 4.
 *******************************************************************************/
+$num1 = 5;
+$num2 = 7;
+$num3 = 8;
+$num4 = 10;
 
+$peso1 = 1;
+$peso2 = 2;
+$peso3 = 3;
+$peso4 = 4;
+
+$somaProdutos = ($num1 * $peso1) + ($num2 * $peso2) + ($num3 * $peso3) + ($num4 * $peso4);
+$somaPesos = $peso1 + $peso2 + $peso3 + $peso4;
+$mediaPonderada = $somaProdutos / $somaPesos;
+
+echo "A média ponderada é: $mediaPonderada\n";
+/*Resumindo:Esse sistema lê quatro números inteiros, multiplica cada um pelos seus respectivos pesos, divide a soma total dos produtos pela soma dos pesos e exibe a média ponderada resultante.*/
+?>
